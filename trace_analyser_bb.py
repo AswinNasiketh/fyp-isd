eb180b514b63f32e0120a8d94cace95d29bec016
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2020-11-28 17:48:30 +0000

    Complete branch profiling in trace

--- trace_analyser_bb.py
+++ trace_analyser_bb.py
@@ -1,7 +1,13 @@
-#Basic Block Sequence Identifier
+#Backward Branch Sequence Identifier
 
+#64 bit hexadecimal string to 64 bit signed integer
+def hex2sint(hex):
+    dec_num = int(hex, base=16)
+    if dec_num >= 0x8000000000000000:
+        dec_num -= 0x10000000000000000
 
-branches = []
+    return dec_num
+
 
 class BranchProfileEntry:
 
@@ -13,77 +19,144 @@ class BranchProfileEntry:
     def increment_taken_count(self):
         self.num_branch_taken += 1
 
-def is_branch_cached(addr):
-    for branch_profile in branches:
-        if branch_profile.branch_instr_addr == addr:
-            return branch_profile
 
-    return False
+class BranchProfile:
+    def __init__(self):
+        self.branches = []
 
-def add_new_branch_profile(instr_addr, target):
-    branches.append(BranchProfileEntry(instr_addr, target))
+        self.branch_opcodes = [
+                    'c.j',
+                    'c.jal',
+                    'c.jr',
+                    'c.jalr',
+                    'c.beqz',
+                    'c.bnez',
+                    'jal',
+                    'jalr',
+                    'j',
+                    'beq',
+                    'bne',
+                    'blt',
+                    'bge',
+                    'bltu',
+                    'bgeu'
+        ]
 
+        self.process_next_iter = False
+        self.addr_next_iter = ""
 
-branch_opcodes = [
-    'c.j',
-    'c.jal',
-    'c.jr',
-    'c.jalr',
-    'c.beqz',
-    'c.bnez',
-    'jal',
-    'jalr',
-    'j',
-    'beq',
-    'bne',
-    'blt',
-    'bge',
-    'bltu',
-    'bgeu'
-    ]
+    def is_branch_cached(self, addr):
+        for branch_profile in self.branches:
+            if branch_profile.branch_instr_addr == addr:
+                return branch_profile
 
+        return False
 
+    def add_new_branch_profile(self, instr_addr, target):
+        self.branches.append(BranchProfileEntry(instr_addr, target))
 
+    def process_on_next_line(self, addr):
+        self.process_next_iter = True
+        self.addr_next_iter = addr
 
-process_next_iter = False
-opcode_next_iter = ""
-addr_next_iter = ""
-target_next_iter = ""
-
-with open('trace_files/sim3.trace', 'r') as reader:
-    for line in reader:
+    def process_trace_line(self, line):
         fields = line.split() #splits line into list, delimiter is spaces by default
 
         instr_addr = fields[0].split('=')[1] 
+        # print(instr_addr)
 
         #processing for previous line if required
-        if process_next_iter:
-            process_next_iter = False
-            if opcode_next_iter == "jalr":
-                add_new_branch_profile(addr_next_iter, instr_addr)           
+        if self.process_next_iter:
+            self.process_next_iter = False
+            self.add_new_branch_profile(self.addr_next_iter, instr_addr)           
            
     
-        branch_entry = is_branch_cached(instr_addr)
+        branch_entry = self.is_branch_cached(instr_addr)
         if branch_entry != False:
             branch_entry.increment_taken_count()
         else:
             #check if backward branch and add to list
             opcode = fields[1]
+            
+            if opcode in self.branch_opcodes:
+                # print("new instr found opcode:", instr_addr )
+                if opcode == "c.j" or opcode == "c.jal" or opcode == "c.jr" or opcode == "c.jalr" or opcode == "jalr" or opcode == "jal" or "j": #unconditional branches
+                    self.process_on_next_line(instr_addr)
+                elif opcode == "c.beqz":
+                    rs1_val = fields[-1].split('=')[1]
+                    if rs1_val == 0:
+                        self.process_on_next_line(instr_addr)
+                
+                elif opcode == "c.bnez":
+                    rs1_val = fields[-1].split('=')[1]
+                    if rs1_val != 0:
+                        self.process_on_next_line(instr_addr)
+                
+                elif opcode == "beq":
+                    rs1_val = fields[-2].split('=')[1]
+                    rs2_val = fields[-1].split('=')[1]
 
-            branch_target = ""
+                    if rs1_val == rs2_val:
+                        self.process_on_next_line(instr_addr)
+                
+                elif opcode == "bne":
+                    rs1_val = fields[-2].split('=')[1]
+                    rs2_val = fields[-1].split('=')[1]
 
-            if opcode in branch_opcodes:
-                if opcode == "c.jr" or opcode == "c.jalr":
-                    branch_target = fields[-1].split('=')[1] #annotated rs1_val
-                    add_new_branch_profile(instr_addr, branch_target)
-                elif opcode == "jalr":
-                    process_next_iter = True
-                    opcode_next_iter = opcode
-                    addr_next_iter = instr_addr
+                    if rs1_val != rs2_val:
+                        self.process_on_next_line(instr_addr)
 
+                elif opcode == "blt":
+                    rs1_val = fields[-2].split('=')[1]
+                    rs2_val = fields[-1].split('=')[1]
 
+                    #by default python converts hex to uint
+                    
+                    if hex2sint(rs1_val) < hex2sint(rs2_val):
+                        self.process_on_next_line(instr_addr)
+                elif opcode == "bge":
 
+                    rs1_val = fields[-2].split('=')[1]
+                    rs2_val = fields[-1].split('=')[1]
+                    
+                    if hex2sint(rs1_val) >= hex2sint(rs2_val):
+                        self.process_on_next_line(instr_addr)
+                
+                elif opcode == "bltu":
+                    rs1_val = fields[-2].split('=')[1]
+                    rs2_val = fields[-1].split('=')[1]
+                    
+                    if rs1_val < rs2_val:
+                        self.process_on_next_line(instr_addr)
     
-        
 
+                elif opcode == "bgeu":
+                    rs1_val = fields[-2].split('=')[1]
+                    rs2_val = fields[-1].split('=')[1]
+                    
+                    if rs1_val >= rs2_val:
+                        self.process_on_next_line(instr_addr)
+
+    def print_profile(self):
+        for entry in self.branches:
+            print("Branch Instr Addr:", entry.branch_instr_addr,
+            "Branch Target:", entry.branch_target,
+            "Number of times taken:", entry.num_branch_taken)
+
+    def dump_profile(self):
+        with open("branch.profile", 'w') as writer:
+            for entry in self.branches:
+                write_string = "Branch Instr Addr: " + entry.branch_instr_addr + ", Branch Target: " + entry.branch_target + ", Number of times taken: " + str(entry.num_branch_taken) + "\n"
+                writer.write(write_string)
+
+def main():
+    branch_profile = BranchProfile()
+
+    with open('trace_files/sim4.trace', 'r') as reader:
+        for line in reader:
+            branch_profile.process_trace_line(line)
     
+    print("Profiling done:")
+    branch_profile.dump_profile()
+
+main()
